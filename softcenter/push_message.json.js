8bd6725bd55995d7850ac84eb200a0533cb1707f
sadoneli <sadoneli@gmail.com>
2018-05-02 10:47:21 +0800

    software_center: update push message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,7 +1,7 @@
 {
 "title":"<em>梅林改版固件&nbsp;-&nbsp;软件中心</em>",
 "content1":"<b>通知：</b>",
-"content2":"koolshare merlin改版固件X7.8已经发布。<br />请前往论坛<a href='http://koolshare.cn/forum-96-1.html' target='_blank'> koolshare.cn</a>更新吧~",
+"content2":"koolshare merlin改版固件X7.8已经发布，请前往论坛<a href='http://koolshare.cn/forum-96-1.html' target='_blank'> koolshare.cn</a>更新吧~",
 "content3":"软件中心1.3.9更新日志：新增对Enable JFFS custom scripts and configs选项是否开启的检测。建议升级！",
 "content4":""
 }
