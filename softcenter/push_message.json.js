82ae4e37478a871e140714f896407897ccaf2145
sadoneli <sadoneli@gmail.com>
2018-05-02 10:49:39 +0800

    software_center: update push message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,7 +1,11 @@
 {
 "title":"<em>梅林改版固件&nbsp;-&nbsp;软件中心</em>",
-"content1":"<b>通知：</b>",
-"content2":"koolshare merlin改版固件X7.8已经发布，请前往论坛<a href='http://koolshare.cn/forum-96-1.html' target='_blank'> koolshare.cn</a>更新吧~",
-"content3":"软件中心1.3.9更新日志：新增对Enable JFFS custom scripts and configs选项是否开启的检测。建议升级！",
+"content1":"<b>通知：</b>koolshare merlin改版固件X7.8已经发布，请前往论坛<a href='http://koolshare.cn/forum-96-1.html' target='_blank'> <em>http://koolshare.cn</em> </a>更新吧~",
+"content2":"软件中心1.3.9更新日志：新增对Enable JFFS custom scripts and configs选项是否开启的检测。建议升级！",
+"content3":"<font color='#1E90FF'>交流反馈:&nbsp;&nbsp;</font>\
+<a href='https://github.com/koolshare/koolshare.github.io' target='_blank'><em>1.软件中心GitHub项目</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
+<a href='https://t.me/xbchat' target='_blank'><em>2.加入telegram群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
+<a href='http://shang.qq.com/wpa/qunwpa?idkey=f475468129ba8019245425559b5df5bdad7d7201ac7780417dd0218bbb4e1322' target='_blank'><em>3.加入QQ群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
+<a href='http://koolshare.cn/forum-98-1.html' target='_blank'><em>4.Koolshare论坛插件版块</em></a>",
 "content4":""
 }
