9159e5d04576911bde8d5ea56e3c93f1e24bbfde
sadoneli <sadoneli@gmail.com>
2018-05-13 19:54:13 +0800

    shadowsocks: 3.9.9

--- shadowsocks/config.json.js
+++ shadowsocks/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"3.9.8",
-"md5":"15ab0a50e480a78a45e6aaa154ae85db",
+"version":"3.9.9",
+"md5":"4108455cf5bd256ceef06b9486e425d5",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2018-05-05_00:20:39"
+"build_date":"2018-05-13_19:53:57"
 }
